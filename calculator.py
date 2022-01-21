b6564687080eb75240b59ed9ec462f518c78cc9c
FreestyleCoders <pythons8376@hotmail.com>
2022-01-21 16:06:40 +0530

    Calculator 1.2

--- calculator.py
+++ calculator.py
@@ -29,6 +29,7 @@ def cirarea():
 
 def prime():
     n = int(input("primes till: "))
+    
 
     primes = reduce(lambda r, x: r - set(range(x**2, n, x))
                     if x in r else r, range(2, int(n**0.5) + 1), set(range(2, n)))
